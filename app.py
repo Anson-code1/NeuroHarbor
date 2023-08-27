5dd15555f453f6da673db548f7ac10cacb370dba
Anson-code1 <143233526+Anson-code1@users.noreply.github.com>
2023-08-26 23:46:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,5 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-ClarifaiStreamlitCSS.insert_default_css(st)
-
 st.markdown("Please select a specific page from the sidebar to the left")
 
