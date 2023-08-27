f5b22bb93bcb95e697eecca07d7c10649c7d6bd5
Anson-code1 <143233526+Anson-code1@users.noreply.github.com>
2023-08-27 18:50:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import llama-llms
+import llama-llm
 from streamlit_chat import message
 from clarifai.auth.helper import ClarifaiAuthHelper
 from clarifai.modules.css import ClarifaiStreamlitCSS
@@ -49,7 +49,7 @@ if user_prompt:
     
     message(user_prompt, is_user=True)
 
-    response = llama.get_response(user_prompt)  # get response from llama2 API (in our case from Workflow we created before)
+    response = llama-llm.get_response(user_prompt)  # get response from llama2 API (in our case from Workflow we created before)
 
     msg = {"role": "assistant", "content": response}
 
