288c08a37a54bc48c9baa85e62c6e1bf529f95ae
Anson-code1 <143233526+Anson-code1@users.noreply.github.com>
2023-08-27 18:49:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import llama
+import llama-llms
 from streamlit_chat import message
 from clarifai.auth.helper import ClarifaiAuthHelper
 from clarifai.modules.css import ClarifaiStreamlitCSS
